91bd8f413f08d7f7d9593c1f9dd9f7d781316068
Hugues Gouttebroze <55590605+gouttebroze@users.noreply.github.com>
2026-05-17 13:47:28 +0000

    graph_html

--- app.py
+++ app.py
@@ -8,3 +8,20 @@ figure = px.pie(données, values='qte', names='region', title='quantité vendue 
 figure.write_html('ventes-par-region.html')
 
 print('ventes-par-région.html généré avec succès !')
+
+figure2 = px.pie(données, values='qte', names='produit', title='quantité vendue par région')
+
+figure2.write_html('ventes-par-produit.html')
+
+print('ventes-par-produit.html généré avec succès !')
+
+print(données.columns.tolist())
+print(données.head().to_string())
+
+ca = données['qte'] * données['prix']
+données['ca'] = ca
+figure3 = px.pie(données, values='ca', names='produit', title='chiffre d affaires par produit')
+
+figure3.write_html('ca-par-produit.html')
+
+print('ca-par-produit.html généré avec succès !')
